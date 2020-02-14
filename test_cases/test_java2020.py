f42fd8bc232d06664590fa4b660f9086a26570df
Lily Huang <945349588@qq.com>
2020-02-14 20:59:11 +0800

    ll

--- test_cases/test_java2020.py
+++ test_cases/test_java2020.py
@@ -12,12 +12,7 @@ suit = unittest.defaultTestLoader.discover(test_dir,pattern="test*.py")
 class Test_Java2020(unittest.TestCase):
 	def test_java(self):
 		#dr = webdriver.Chrome('C:\\Users\\Lily\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe')
-		chrome_options = webdriver.ChromeOptions()
-		chrome_options.add_argument('--headless')
-		chrome_options.add_argument('--no-sandbox')
-		chrome_options.add_argument('--disable-gpu')
-		chrome_options.add_argument('--disable-dev-shm-usage')
-		dr = webdriver.Chrome(options=chrome_options)
+		dr = webdriver.Chrome('/usr/bin/chromedriver.exe')
 		dr.get('http://localhost:8088/router.html#/')
 		dr.implicitly_wait(10)
 		dr.maximize_window()
