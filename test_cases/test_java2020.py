3253b6b6e2b743ee099163ffabaf63321127eb79
Lily Huang <945349588@qq.com>
2020-02-14 18:53:22 +0800

    modify

--- test_cases/test_java2020.py
+++ test_cases/test_java2020.py
@@ -4,22 +4,22 @@ import unittest
 import time
 from HTMLTestRunner import HTMLTestRunner
 
-# 定义测试用例所在目录
+#定义测试用例所在目录
 test_dir = './test_cases'
 suit = unittest.defaultTestLoader.discover(test_dir,pattern="test*.py")
 
 class Test_Java2020(unittest.TestCase):
-    def test_java(self):
-        #dr = webdriver.Chrome('C:\\Users\\Lily\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe')
+	def test_java(self):
+		#dr = webdriver.Chrome('C:\\Users\\Lily\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe')
 		dr = webdriver.Chrome()
 		dr.get('http://localhost:8088/router.html#/')
-        dr.implicitly_wait(10)
-        dr.maximize_window()
-        new_reg = dr.find_element_by_xpath('//*[@id="app"]/div/a[1]/button')
-        new_reg_txt = new_reg.text
-        self.assertEqual(new_reg_txt,"注册新用户","注册新用户失败！！！")
-        print(u"注册新用户成功！")
-        print(u"This is Java2020 AutoTest Project!")
-        dr.quit()
+		dr.implicitly_wait(10)
+		dr.maximize_window()
+		new_reg = dr.find_element_by_xpath('//*[@id="app"]/div/a[1]/button')
+		new_reg_txt = new_reg.text
+		self.assertEqual(new_reg_txt,"注册新用户","注册新用户失败！！！")
+		print(u"注册新用户成功！")
+		print(u"This is Java2020 AutoTest Project!")
+		dr.quit()
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+	unittest.main(verbosity=2)
